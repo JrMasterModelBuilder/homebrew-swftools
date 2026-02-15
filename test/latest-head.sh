8bcd793878b7a3208022efbb470e310207602f6d
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2026-02-14 23:08:27 -0500

    Use env

--- test/latest-head.sh
+++ test/latest-head.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+expected="${SWFTOOLS_HEAD}"
+
 tmpclone='/tmp/matthiaskramm-swftools-latest-check'
 rm -rf "${tmpclone}"
 git clone --depth 1 'https://github.com/matthiaskramm/swftools.git' "${tmpclone}" 2> /dev/null
@@ -10,8 +12,6 @@ pushd "${tmpclone}" > /dev/null
 commit="$(git rev-parse HEAD)"
 popd > /dev/null
 rm -rf "${tmpclone}"
-
-expected='c6a18ab0658286f98d6ed2b3d0419058e86a14a0'
 
 if [[ "${commit}" == "${expected}" ]]; then
 	echo 'HEAD commit on master unchanged'
