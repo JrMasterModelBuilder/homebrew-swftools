22fa9da6a831fbd995d01c48d7de4636829c645b
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2026-09-01 23:45:22 -0400

    Define YY_USE_PROTOS

--- Formula/swftools-head.rb
+++ Formula/swftools-head.rb
@@ -25,6 +25,7 @@ class SwftoolsHead < Formula
     ENV["PYTHON_LIB"] = "/dev/null"
     ENV["PYTHON_INCLUDES"] = "/dev/null"
     ENV["RUBY"] = "/dev/null"
+    ENV.append "CFLAGS", "-DYY_USE_PROTOS"
     args = []
     args << "--build=aarch64-unknown-linux-gnu" if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     system "./configure", *args, *std_configure_args
